993841f1a4698effb858f3e6232bdfe937c4ae54
Levi Miller <paullevimiller@gmail.com>
2019-07-19 12:24:57 -0700

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
   # code here
+
+  sorted_strings = arr.sort_by { |string| string.chars.map{|char| ESPERANTO_ALPHABET.index(char)}
+  }
+
+  return sorted_strings
 end
